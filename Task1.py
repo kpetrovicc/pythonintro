6579098ce51ad20787f7afae3288e96a16d48952
Kaya <petrovickatarina684@gmail.com>
2024-12-05 02:51:51 +0000

    add

--- Task1.py
+++ Task1.py
@@ -1,4 +1,11 @@
 name = "John"
 age = 35
 
-print("There once was a man named "
+print("There once was a man named " + name + ",")
+print("he was " + str(age) + " years old.")
+
+name = "Mike"
+age = 56
+
+print("He really liked the name " + name + ",")
+print("but didn't like being " + str(age) + ".")
