7618c06baac92fd7ff4e491c0020daa60ff6ee48
Kaya <petrovickatarina684@gmail.com>
2024-12-10 23:54:33 +0100

    add

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 # import useful_tools as ut
 
-# print(ut.roll_dice(
+# print(ut.roll_dice(10))
+
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.4, True)
+student2 = Student("Pam", "Art", 2.5, False)
+print(student1.name + " is a " + student1.major + " major with a GPA of " + str(student1.gpa) + (" and is on probation: " if student1.is_on_probation else " is not on probation."))
+print(student2.name + " is a " + student2.major + " major with a GPA of " + str(student2.gpa) + (" and is on probation: " if student2.is_on_probation else " is not on probation."))
+
+print(student1.name +" "+ ("is" if student1.on_honor_roll() else "is not") + " on the honor roll.")
