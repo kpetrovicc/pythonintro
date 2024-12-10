c6a542b0c73c56822b491c3c4910912214fc3d2f
Kaya <petrovickatarina684@gmail.com>
2024-12-10 23:54:13 +0100

    add

---
+++ Student.py
@@ -0,0 +1,14 @@
+class Student:
+    # we want to define a bunch of attributes to describe a student
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
+    
---
+++ app.py
@@ -0,0 +1,3 @@
+# import useful_tools as ut
+
+# print(ut.roll_dice(
