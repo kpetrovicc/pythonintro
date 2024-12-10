f8068e852cde32b243d778522258db341e870fcd
Kaya <petrovickatarina684@gmail.com>
2024-12-10 18:10:50 +0100

    add

---
+++ ForLoop.py
@@ -0,0 +1,9 @@
+# for letter in "Giraffe Academy":
+#     print(letter)
+
+# friends = ["Jim", "Karen", "Kevin"]
+# for friend in friends:
+#     print(friend)
+
+for index in range(10):
+    print(index)
